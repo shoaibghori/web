45a3c2d2dc7f3dabf58ffae9d333d247647218d0
ShoaibGhori <msghori.dev@gmail.com>
2025-06-02 12:39:45 +0500

    fix: modifications

--- portfolio/arcana/js/scorll.js
+++ portfolio/arcana/js/scorll.js
@@ -1,6 +1,30 @@
+// smooth scroll for contact us 
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+// smooth scroll for contact us 
+
+// scroll to bottom top
+       const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+                scrollToTopBtn.style.display = "block";
+            } else {
+                scrollToTopBtn.style.display = "none";
+            }
+        };
+
+        // When the user clicks on the button, scroll to the top of the document
+        scrollToTopBtn.addEventListener("click", function () {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+
+        // scroll to bottom top
